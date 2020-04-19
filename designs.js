06dec4f9fbdea6e08885805544ac69e73220ca7e
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-04-19 09:39:22 +0200

    changes requested by reviewer
    refactor: simpler code for removing previous canvas
    docs: major parts of the code are now commented

--- designs.js
+++ designs.js
@@ -1,23 +1,19 @@
+
 let grid = document.querySelector('table');
 let color = document.getElementById('colorPicker');
 let button = document.querySelector('[type=submit]');
 
 button.addEventListener('click', submitClick);
 
-let firstSubmit = false;
 
-function submitClick(event) {
-    event.preventDefault();
-    
-	if (firstSubmit) {
-        while (grid.firstChild) {
-			grid.firstChild.remove();
-		}
-	}
-    firstSubmit = true;
-    makeGrid();
+function submitClick(event) {    
+	event.preventDefault();
+    // remove previous grid before creating a new one
+	document.querySelector('#pixelCanvas').innerHTML = ''  
+	makeGrid();
 }
 
+// function that creates grid with height and width specified by user
 function makeGrid() {
     let height = document.querySelector('#inputHeight').value;
     let width = document.querySelector('#inputWidth').value;  
@@ -26,6 +22,7 @@ function makeGrid() {
         grid.appendChild(tr);
         for (let j = 1; j <= width; j++) {
             let td = document.createElement('td');
+			// change cell background to color chosen by user
 			td.addEventListener('click', (event) => {
 				td.style.backgroundColor = color.value;
 			});
